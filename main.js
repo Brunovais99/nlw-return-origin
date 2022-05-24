0bc86ddcbcd99232f6f33d3611d0b9459775459b
Bruno <novais99bruno@gmail.com>
2022-05-23 23:49:55 -0300

    Trabalhando na responsividade

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+    showNavOnScroll()
+    showBackToTopButton()
+}
+
+function showNavOnScroll() {
     if(scrollY > 0) {
         navigation.classList.add("scroll");
     } else {
         navigation.classList.remove("scroll"); 
+    }
+}
+
+function showBackToTopButton() {
+    if(scrollY > 600) {
+        backToTopButton.classList.add("show");
+    } else {
+        backToTopButton.classList.remove("show"); 
     }
 }
 
@@ -14,3 +31,10 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove("menuExpanded");
 }
+
+
+
+ScrollReveal({
+    origin: 'top',
+    duration: 1000,
+}).reveal(`#home, #home img, #home .stats, #services, #services header, #services .cards, #about, #about header, #about .content, #contact, #contact header, #contact .content`);
